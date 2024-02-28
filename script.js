bd7954b26b74d0c18e1abd6492cedda6d5b6a2b1
Ramadan Cilomba <rcilomba@yahoo.com>
2024-02-28 21:49:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const scoreCounter = document.querySelector(".score-counter");
+const grid = document.querySelector(".grid");
+const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
+const playAgainButton = document.querySelector(".play-again");
